327da7f01fd2007c0b168bc5a4c537e4e9d72a3f
Ashutosh Subedi <97485279+AshSubedi7@users.noreply.github.com>
2023-04-05 13:27:41 -0400

    Add files via upload
    This is the bulk of the main game files, main.py, sprites.py, and tilemap.py.

---
+++ main.py
@@ -0,0 +1,140 @@
+import pygame
+from pygame import mixer
+from sprites import *
+from config import *
+from tilemap import *
+import sys
+
+pygame.mixer.init()
+music = pygame.mixer.music.load('fantasy_music.ogg')
+pygame.mixer.music.play(-1)
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.font = pygame.font.Font('super_legend_boy_font.ttf', 32)
+
+        self.character_spritesheet = Spritesheet('img/main_char.png')
+        self.terrain_spritesheet = Spritesheet('img/terrain.png')
+        self.enemy_spritesheet = Spritesheet('img/enemy.png')
+        self.intro_background = pygame.image.load('img/bread_pixel.png')
+        self.go_background = pygame.image.load('./img/gameover.png')
+
+    def createTilemap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                Ground(self, j, i)
+                if column == "B":
+                    Block(self, j, i)
+                if column =="H":
+                    Enemy_horizontal(self, j, i)
+                if column == "V":
+                    Enemy_vertical(self, j, i)
+                if column == "P":
+                    Player(self, j, i)
+
+    def new(self):
+        
+        # a new game starts
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.createTilemap()
+
+    def events(self):
+        #game loop events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        #game loop updates
+        self.all_sprites.update()
+
+    def draw(self):
+        # game loop draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        #game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+
+    def game_over(self):
+        text = self.font.render('Game Over', True, WHITE)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+        restart_button = Button(10, WIN_HEIGHT - 60, 200, 50, WHITE, BLACK, 'Restart', 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            self.screen.blit(self.go_background, (0,0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
+    def intro_screen(self):
+        intro = True
+
+        title = self.font.render('PAN-dora', True, BLACK)
+        title_rect = title.get_rect(x = 10, y = 10)
+
+        play_button = Button(10, 50, 200, 45, RED, BLACK, 'Play PAN-dora', 18)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+
+            self.screen.blit(self.intro_background, (7,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
+
+
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit
+
+
